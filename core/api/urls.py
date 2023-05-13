0ddabef723c99cf1eae12f6dca32af744704d358
PronoyJoy <pronoy.das.developer@gmail.com>
2023-05-14 00:18:05 +0600

    added registration ,login url

--- core/api/urls.py
+++ core/api/urls.py
@@ -4,23 +4,22 @@ from rest_framework.authtoken import views
 from asset_track.views import (
     CompanyView,EmployeeView,
     DeviceView,DeviceLogView,
-    RegisterView,LoginView
-    )
+    RegisterView,LoginView)
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'companies',CompanyView,basename='companies')
 router.register(r'employees',EmployeeView,basename='employees')
-router.register(r'device',DeviceView,basename='device')
+router.register(r'devices',DeviceView,basename='devices')
 router.register(r'device_logs',DeviceLogView,basename='device_logs')
 
 
 urlpatterns = [
 
     path('',include(router.urls)),
-    path('register',RegisterView.as_view()),
-    path('login',LoginView.as_view())
-    # path('api-token-auth/', views.obtain_auth_token,name='create-token'),
+    path('register/',RegisterView.as_view()),
+    path('login/',LoginView.as_view())
+   
     
 
 ]
