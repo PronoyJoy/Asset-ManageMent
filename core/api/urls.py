595a405479adb92eefd9da1435b9ebed18fbb7b4
PronoyJoy <pronoy.das.developer@gmail.com>
2023-05-13 19:34:07 +0600

    added register and login view in urls

--- core/api/urls.py
+++ core/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path,include
+from rest_framework.authtoken import views
 
 from asset_track.views import (
     CompanyView,EmployeeView,
-    DeviceView,DeviceLogView
+    DeviceView,DeviceLogView,
+    RegisterView,LoginView
     )
 from rest_framework.routers import DefaultRouter
 
@@ -16,5 +18,9 @@ router.register(r'device_logs',DeviceLogView,basename='device_logs')
 urlpatterns = [
 
     path('',include(router.urls)),
+    path('register',RegisterView.as_view()),
+    path('login',LoginView.as_view())
+    # path('api-token-auth/', views.obtain_auth_token,name='create-token'),
+    
 
 ]
